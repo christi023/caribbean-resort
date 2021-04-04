b63215333d5a0ffa8c5dac2b8a1e93bfca12c514
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-04-04 14:34:07 +0200

    Added BrowserRouter from react-dom for routing the pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Pages
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Rooms from './Pages/Rooms/Rooms';
+import SingleRoom from './Pages/SingleRoom/SingleRoom';
+import Bookings from './Pages/Bookings/Bookings';
+import Error from './Pages/Error/Error';
+import Contact from './Pages/Contact/Contact';
+// style
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Caribbean Resort</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/rooms" component={Rooms} />
+          <Route exact path="/room" component={SingleRoom} />
+          <Route exact path="/bookings" component={Bookings} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={Error} />
+        </Switch>
+      </Router>
     </div>
   );
 }
