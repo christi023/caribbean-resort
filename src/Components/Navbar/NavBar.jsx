33c0d02aa957a054bd0b7cb3ca40e7a893342530
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-04-05 16:34:05 +0200

    Navbar, styles and media queries

---
+++ src/Components/Navbar/NavBar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { navData } from '../../data/NavData';
+import { Link } from 'react-router-dom';
+//import { Link, animateScroll as scroll } from 'react-scroll';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import $ from 'jquery';
+
+// styles
+import './Navbar.css';
+
+// for changing navbar  color
+$(window).scroll(function () {
+  $('Navbar').toggleClass('scrolled', $(this).scrollTop() > 0);
+});
+
+export default function NavBar() {
+  /*const scrollToTop = () => {
+    scroll.scrollToTop();
+  };*/
+  return (
+    <>
+      <Navbar bg="light" variant="dark" expand="lg" collapseOnSelect className="navbar-fixed-top">
+        <Container>
+          <Navbar.Brand>
+            <Link to="/">
+              <span className="navbar-brand font-weight-bolder">Carib Suite</span>
+            </Link>
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              {navData.map((item, i) => (
+                <Link
+                  to={item.link}
+                  key={i}
+                  className="nav-menu-links"
+                  activeClassName="active_class"
+                >
+                  {item.title}
+                </Link>
+              ))}
+              <br />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
