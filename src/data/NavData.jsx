8b06014e01fa82521ca8ad8de17165fef3254328
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-04-06 20:14:31 +0200

    Holds navbar data

---
+++ src/data/NavData.jsx
@@ -0,0 +1,22 @@
+export const navData = [
+  {
+    title: 'Home',
+    link: '/',
+  },
+  {
+    title: 'About',
+    link: '/about',
+  },
+  {
+    title: 'Rooms',
+    link: '/rooms',
+  },
+  {
+    title: 'Contact ',
+    link: '/contact',
+  },
+  {
+    title: 'Sign In ',
+    link: '/register',
+  },
+];
