21b3bcc92b82ef3e77e02f8a9c0b808bdc1c4f49
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-04-06 20:10:23 +0200

    Rooms page

--- src/Pages/Rooms/Rooms.jsx
+++ src/Pages/Rooms/Rooms.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+// Components
+import Hero2 from '../../Components/Hero2/Hero2';
+import Banner from '../../Components/Banner/Banner';
 
 export default function Rooms() {
   return (
-    <div>
-      <h1>Rooms</h1>
-    </div>
+    <>
+      <Hero2 hero2="roomsHero">
+        <Banner title="our rooms">
+          <Link to="/" className="btn btn-primary">
+            return home
+          </Link>
+        </Banner>
+      </Hero2>
+    </>
   );
 }
