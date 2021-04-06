4c73cdf1c680c301d97b0f6bbc9e64715c0de7cb
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-04-06 20:07:55 +0200

    404 Error page

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Hero from '../../Components/Hero/Hero';
+import Banner from '../../Components/Banner/Banner';
+import { Link } from 'react-router-dom';
 
 export default function Error() {
   return (
-    <div>
-      <h1>Error Page</h1>
-    </div>
+    <Hero>
+      <Banner title="404" subtitle="page not found">
+        <Link to="/" className="btn btn-primary">
+          Return Home
+        </Link>
+      </Banner>
+    </Hero>
   );
 }
