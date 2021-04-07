69aaf59419467254772cfad5359222e6e36dc225
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-04-07 18:18:44 +0200

    Banner added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Hero from '../../Components/Hero/Hero';
+import Banner from '../../Components/Banner/Banner';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <Hero>
+        <Banner title="Exotic Rooms" subtitle="Deluxe rooms starting at $275">
+          <Link to="/rooms" className="btn btn-primary">
+            Our Rooms
+          </Link>
+        </Banner>
+      </Hero>
+    </>
   );
 }
