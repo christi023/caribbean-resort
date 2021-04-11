b263196018a35554fdedf71c543084f73fbad232
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-04-11 15:05:23 +0200

    holds the services data information

---
+++ src/data/ServicesData.jsx
@@ -0,0 +1,26 @@
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa';
+
+const services = [
+  {
+    icon: <FaCocktail />,
+    title: 'free cocktails',
+    info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    icon: <FaHiking />,
+    title: 'endless hiking',
+    info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    icon: <FaShuttleVan />,
+    title: 'free shuttle',
+    info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    icon: <FaBeer />,
+    title: 'strongest beer',
+    info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+];
+
+export default services;
