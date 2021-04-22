3fecfd9bc3547acaf6a0950c18609490d824af95
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-04-22 17:41:21 +0200

    loading gif for loading rooms

---
+++ src/Components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import loading from '../../images/gif/loading-arrow.gif';
+
+export default function Loading() {
+  return (
+    <div className="loading">
+      <h4>Rooms data loading...</h4>
+      <img src={loading} alt="Loading Please Wait" />
+    </div>
+  );
+}
