4d983bee41e785e178b3ee8b206fa6eccacbd968
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-04-27 10:37:10 +0200

    fixed css media queries

--- src/Components/Room/Room.jsx
+++ src/Components/Room/Room.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Col } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
 import defaultImg from '../../images/room-1.jpeg';
-
 // styles
 import './Room.css';
+
 export default function Room({ room }) {
   return (
-    <Col md={4} col={12} className="mx-auto p-2">
-      <Card className="shadow-lg border-0 room">
-        <img
-          src={(room.images && room.images[0]) || defaultImg}
-          alt="single room"
-          className="img-fluid"
-        />
-        <div className="price-top">
-          <h6>${room.price}</h6>
-          <p>per night</p>
+    <>
+      <article className="room">
+        <div className="img-container">
+          <img
+            src={(room.images && room.images[0]) || defaultImg}
+            alt="single room"
+            className="img-fluid"
+          />
+          <div className="price-top">
+            <h6>${room.price}</h6>
+            <p>per night</p>
+          </div>
+          <Link
+            to={`/rooms/${room.slug}`}
+            className="btn-warning room-link text-center"
+            style={{ marginRight: '-2rem' }}
+          >
+            Features
+          </Link>
+          <p className="room-info">{room.name}</p>
         </div>
-        <Link to={`/rooms/${room.slug}`} className="btn-warning room-link text-center">
-          Features
-        </Link>
-        <p className="room-info">{room.name}</p>
-      </Card>
-    </Col>
+      </article>
+    </>
   );
 }
